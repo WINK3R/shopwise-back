0c655693e883046fb968380b2a43330b3b503d83
ldelanier <ldelanier@cikaba.com>
2026-07-03 15:11:32 +0200

    Initial commit

---
+++ src/main/java/com/example/shopwiseapi/ShopwiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopwiseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopwiseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopwiseApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shopwiseapi/ShopwiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopwiseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopwiseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
